31f08f5e413a2dee394399980897a1115cee23ce
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-27 14:48:26 +0500

    dynamodb func

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 type Todo struct {
@@ -13,14 +20,58 @@ type Todo struct {
 	Status      bool   `json:"status" dynamodbav:"status"`
 }
 
+const TableName = "Todos"
+
+var db dynamodb.Client
+
+func init() {
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = *dynamodb.NewFromConfig(sdkConfig)
+}
+
+func listItems(ctx context.Context) ([]Todo, error) {
+	todos := make([]Todo, 0)
+	var token map[string]types.AttributeValue
+
+	for {
+		input := &dynamodb.ScanInput{
+			TableName:         aws.String(TableName),
+			ExclusiveStartKey: token,
+		}
+
+		result, err := db.Scan(ctx, input)
+		if err != nil {
+			return nil, err
+		}
+
+		var fetchedTodos []Todo
+		err = attributevalue.UnmarshalListOfMaps(result.Items, &fetchedTodos)
+		if err != nil {
+			return nil, err
+		}
+
+		todos = append(todos, fetchedTodos...)
+		token = result.LastEvaluatedKey
+		if token == nil {
+			break
+		}
+	}
+
+	return todos, nil
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/todo", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func main() {

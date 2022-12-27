eda2359db3e8c79f0fb154f00e8922e63eb17200
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-27 14:36:55 +0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ahmed-deftoner/ec2-benchmark
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	Id          string `json:"id" dynamodbav:"id"`
+	Name        string `json:"name" dynamodbav:"name"`
+	Description string `json:"description" dynamodbav:"description"`
+	Status      bool   `json:"status" dynamodbav:"status"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	handleRequests()
+}
